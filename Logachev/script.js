775ef871f4d107e0598f67b03bcf7c2e653da9d1
v1 <kaby.spb1@gmail.com>
2024-09-24 15:01:19 +0300

    2lab done

--- Logachev/script.js
+++ Logachev/script.js
@@ -1,5 +1,5 @@
-function checkbg() {
-
+function myLoad() {
+    if(localStorage["ChisloYchastkov"]>0) {ChisloYchastkov.value=localStorage["ChisloYchastkov"]}
 }
 
 
@@ -76,16 +76,32 @@ function AnomalHide() {
 
 function TableYchastki() {
     let n = ChisloYchastkov.value;
+
+    let a = mytextarea.value.split(/$\s*/m);
+    
+    if (a.length==0) {
+        n=ChisloYchastkov.value;            
+    }
+
+    else {
+        n=a.length;
+        mytextarea.style.height=n*15+'px';
+        ChisloYchastkov.value=n;
+    }
+
+    
     div1.style.display = "block";
     div1.innerHTML = "";
     
     for(let i = 1; i <= n; i++) {
         if (i <= n) {
-            div1.innerHTML = div1.innerHTML + "<input type=button value='-' onClick='AnomalHide(), Calc()' id='Button3'>"
-            div1.innerHTML = div1.innerHTML + "<input type=button value='mid' onClick='AnomalMid(), Calc()' id='Button4'>"
-            div1.innerHTML = div1.innerHTML + "<input type=button value='max' onClick='AnomalMax(), Calc()' id='Button5'>"
-            div1.innerHTML = div1.innerHTML + "<input type=button value='maxA' onClick='AnomalMaxCalc(), Calc()' id='Button6'>"
-            div1.innerHTML += "<div style='display: inline-block; width: 400px;'>Площадь повреждённой территории на " + i + " участке:</div><input type='text' id='ploshad" + i + "'><br>";
+            element1 = "ploshad" + i;
+            
+            if (localStorage[element1] == undefined) {
+                localStorage[element1] = ""
+            }
+            
+            div1.innerHTML += "<div style='display: inline-block; width: 400px;'>Площадь повреждённой территории на " + i + " участке:</div><input type='text' id='ploshad" + i + "' value='"+localStorage[element1]+"'><div id='lolkek" + i + "' style='display:inline-block'></div><br>";
         }
     }
 
@@ -98,6 +114,15 @@ function TableYchastki() {
     div1.innerHTML = div1.innerHTML + "Средняя площадь повреждённой территории: <input type=text id='SredPloshad'><br>"
     div1.innerHTML = div1.innerHTML + "Среднеквадратическое отклонение: <input type=text id='Otklonenie'><br>"
     div1.innerHTML = div1.innerHTML + "Коэффициент: <input type=text id='koef'><br>"
+
+    if (a.length>0) {
+        for (let i=1;i<=n;i++) {
+            element1 = "ploshad" + i;
+            document.getElementById(element1).value = a[i-1];
+        }
+    }
+
+    localStorage["ChisloYchastkov"] = n;    
 }
 
 function Calc() {
@@ -132,6 +157,7 @@ function Calc() {
     for(let i = 1;i <= n; i++) {
         if(i <= n) {
             element1 = "ploshad" + i;
+            localStorage[element1] = document.getElementById(element1).value;
             SumPloshad.value =+ SumPloshad.value+ + document.getElementById(element1).value;
         }
     }
@@ -149,12 +175,20 @@ function Calc() {
 
     for (let i = 1; i <= n; i++) {
             let element1 = "ploshad" + i;
+            let element2 = "lolkek" + i;
             let deviation = (document.getElementById(element1).value - SredPloshad.value) / Otklonenie.value;
             if (deviation >= k) {
                 document.getElementById(element1).style.background = 'red';
+
+                document.getElementById(element2).innerHTML = "<input type=button value='-' onClick='AnomalHide(), Calc()' id='Button${i}'>"
+                + "<input type=button value='mid' onClick='AnomalMid(), Calc()' id='Button${i}'>" 
+                + "<input type=button value='max' onClick='AnomalMax(), Calc()' id='Button${i}'>"
+                + "<input type=button value='maxA' onClick='AnomalMaxCalc(), Calc()' id='Button${i}'>";
+                
             } 
             else {
                 document.getElementById(element1).style.background = '';
+                document.getElementById(element2).innerHTML = '';
             }
         }
 }
